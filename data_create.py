2479f71b44ddcf011bd6ad9054522bff6e155de9
Asputnik <thesteria@mail.ru>
2024-06-25 17:48:35 +0300

    add files project

---
+++ data_create.py
@@ -0,0 +1,11 @@
+def name_data():
+    return input("Введите имя: ")
+
+def surname_data():
+    return input("Введите фамилию: ")
+
+def phone_data():
+    return input("Введите номер телефона: ")
+
+def address_data():
+    return input("Введите адрес: ")
