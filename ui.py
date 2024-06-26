5a3839e48a1216403b31229af098d96f0c083f5e
Asputnik <thesteria@mail.ru>
2024-06-26 11:11:52 +0300

    add Foo print data

--- ui.py
+++ ui.py
@@ -22,3 +22,20 @@ def input_data():
     elif var == 2:
         with open('data_second_variant.txt', 'a', encoding='utf-8') as f:
             f.write(f'{name};{surname};{phone};{address}\n')
+
+def print_data():
+    print("Вывожу данные из файла 1: \n")
+    with open('data_first_variant.csv', 'r', encoding='utf-8') as f:
+        data_first = f.readlines()
+        data_first_list = []
+        j = 0
+        for i in range(len(data_first)):
+            if data_first[i] == '\n' or i == len(data_first) - 1:
+                data_first_list.append(''.join(data_first[j:i+1]))
+                j = i
+        print(''.join(data_first_list))
+
+    print("Вывожу данные из файла 2: \n")
+    with open('data_second_variant.csv', 'r', encoding='utf-8') as f:
+        data_second = f.readlines()
+        print(*data_second)
